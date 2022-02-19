8e010115305c703c0527e975f8d404e0238877cd
PabloCardoso <pablo.silva23@fatec.sp.gov.br>
2022-02-18 22:07:01 -0300

    Atv1

---
+++ TesteCalculadora.java
@@ -0,0 +1,12 @@
+public class TesteCalculadora{
+    
+    public static void main(String[] args){
+        Calculadora n1 = new Calculadora();
+        Calculadora n2 = new Calculadora();
+
+        n1.setNum1(10);
+        n2.setNum2(3);
+
+        System.out.println(testaCalculadora());
+    }
+}
